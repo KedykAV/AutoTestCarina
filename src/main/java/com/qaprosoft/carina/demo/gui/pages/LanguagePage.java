10f6335122b384f418316c5e770c243858d127f1
akedyk <alx.cyberskn@gmail.com>
2021-09-24 16:21:25 +0300

    Added radio-buttons tests

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/LanguagePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/LanguagePage.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class LanguagePage extends AbstractPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(LanguagePage.class);
     @FindBy(xpath = "//span[contains(text(), ' Deutsch ')]")
     private ExtendedWebElement deutsch;
+    @FindBy(css = "input[type='radio']")
+    private List<ExtendedWebElement> radioLangButtons;
     public LanguagePage(WebDriver driver) {
         super(driver);
     }
@@ -21,4 +26,28 @@ public class LanguagePage extends AbstractPage {
 
         return deutsch.getText();
     }
-}
+    public boolean isAnyRadioButtonChecked() {
+        for (ExtendedWebElement radBut : radioLangButtons) {
+            if (radBut.isChecked()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean clickableAllRadioButton(){
+        for(ExtendedWebElement radBut: radioLangButtons){
+            if(radBut.isClickable()){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void clickAllRadioButtons(){
+        SoftAssert softAssert = new SoftAssert();
+        for(int i = 1; i < radioLangButtons.size(); i++){
+             radioLangButtons.get(i).click();
+             softAssert.assertTrue(radioLangButtons.get(i).isChecked());
+        }
+        softAssert.assertAll();
+    }
+    }
--- src/test/java/com/qaprosoft/carina/demo/amazontest/SecondTest.java
+++ src/test/java/com/qaprosoft/carina/demo/amazontest/SecondTest.java
@@ -8,11 +8,12 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 public class SecondTest implements IAbstractTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SecondTest.class);
 
     @BeforeMethod
-    public void openSite(){
+    public void openSite() {
         HomePage homePage = new HomePage(getDriver());
         homePage.open();
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
@@ -48,7 +49,7 @@ public class SecondTest implements IAbstractTest {
         ProductPage productPage = new ProductPage(getDriver());
         productPage.getAddButton().click();
         pause(2);
-        //productPage.getCloseButton().click();
+        productPage.getCloseButton().clickIfPresent();
         productPage.getCartButton().click();
         CartPage cartPage = new CartPage(getDriver());
         cartPage.getDeleteBtn().click();
@@ -73,32 +74,54 @@ public class SecondTest implements IAbstractTest {
         pause(2);
         homePage.clickCcBtn();
         SectionPage sectionPage = new SectionPage(getDriver());
-        sectionPage.getProductList().get((int) (Math.random() * 10) + 1).click();
+        sectionPage.getProductList().get((int) (Math.random() * 10)).click();
         ProductPage productPage = new ProductPage(getDriver());
         productPage.getAddButton().click();
-        productPage.getCloseButton().click();
+        productPage.getCloseButton().clickIfPresent();
         Assert.assertFalse(productPage.getCartButton().isElementWithTextPresent("0"), "Product score don't view");
     }
+
     @Test
-    public void invalidEmail(){
+    public void invalidEmail() {
         HomePage homePage = new HomePage(getDriver());
         homePage.getLoginButton().click();
         AuthorizationPage authorizationPage = new AuthorizationPage(getDriver());
         authorizationPage.getEmailField().type("qwqew@qeqweqwe.com");
         authorizationPage.getContButton().click();
         authorizationPage.getEmailField().getSize();
-        Assert.assertTrue(authorizationPage.getProblemMessage().isElementWithTextPresent("There was a problem"),"You continue with invalid email!");
+        Assert.assertTrue(authorizationPage.getProblemMessage().isElementWithTextPresent("There was a problem"), "You continue with invalid email!");
     }
+
     @Test
-    public void changingDeliverTo(){
+    public void changingDeliverTo() {
         HomePage homePage = new HomePage(getDriver());
         homePage.getChangingDeliverButton().click(2);
         homePage.getListOfCountriesButton().click();
         String selectedCountry = homePage.changeCountry();
-        Assert.assertEquals(homePage.getCurrentCountryName(),selectedCountry,"Don't change current country");
+        Assert.assertEquals(homePage.getCurrentCountryName(), selectedCountry, "Don't change current country");
         homePage.getDoneButton().click();
-        Assert.assertEquals(homePage.getCountryToDelivery(),selectedCountry,"Don't change country to delivery");
+        Assert.assertEquals(homePage.getCountryToDelivery(), selectedCountry, "Don't change country to delivery");
     }
 
-
+    @Test
+    public void isAnyRadioButtonChecked(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.getLangButton().click();
+        LanguagePage languagePage = new LanguagePage(getDriver());
+        Assert.assertTrue(languagePage.isAnyRadioButtonChecked(),"Don't change any radio-button");
+    }
+    @Test
+    public void isAllRadioButtonsClickable(){
+        HomePage homePage=new HomePage(getDriver());
+        homePage.getLangButton().click();
+        LanguagePage languagePage =new LanguagePage(getDriver());
+        Assert.assertTrue(languagePage.clickableAllRadioButton(),"Don't all radio-buttons is clickable");
+    }
+    @Test
+    public void clickAndAssertAllButtons(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.getLangButton().click();
+        LanguagePage languagePage =new LanguagePage(getDriver());
+        languagePage.clickAllRadioButtons();
+    }
 }
